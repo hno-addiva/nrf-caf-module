79226d9dcff0786ac359dfae22192d887b55cb35
Henrik Nordström <henrik.nordstrom@addiva.se>
2022-10-12 22:50:46 +0200

    Drop obsolete BUTTON code template, not relevant.

--- src/modules/module_test_1.c
+++ src/modules/module_test_1.c
@@ -105,26 +105,6 @@ static void module_initialize(void)
 	LOG_DBG("initialized");
 }
 
-#if CONFIG_CAF_BUTTON
-static bool handle_button_event(const struct button_event *evt)
-{
-	if (evt->pressed) {
-		switch (evt->key_id) {
-		case BUTTON_ID_...:
-			...
-			break;
-		case BUTTON_ID_...:
-			...
-			break;
-		default:
-			break;
-		}
-	}
-
-	return false;
-}
-#endif
-
 static bool handle_module_state_event(const struct module_state_event *evt)
 {
 	// Wait for dependent modules to initialize
@@ -137,12 +117,6 @@ static bool handle_module_state_event(const struct module_state_event *evt)
 
 static bool app_event_handler(const struct app_event_header *aeh)
 {
-	#if CONFIG_CAF_BUTTON
-	if (is_button_event(aeh)) {
-		return handle_button_event(cast_button_event(aeh));
-	}
-	#endif
-
 	if (is_module_state_event(aeh)) {
 		return handle_module_state_event(cast_module_state_event(aeh));
 	}
