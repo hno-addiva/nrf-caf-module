c01b8a90ec94da9a7c78f608a5e880cc8abe1397
Henrik Nordström <henrik.nordstrom@addiva.se>
2022-10-12 21:01:22 +0200

    Add a work item and shell command for triggering it

--- src/modules/module_test_1.c
+++ src/modules/module_test_1.c
@@ -15,6 +15,57 @@ LOG_MODULE_REGISTER(MODULE);
 #include "module_test_1.h"
 #include "test_1_def.h"
 
+// Work queue
+static K_THREAD_STACK_DEFINE(stack_area, 2048);
+static struct k_work_q work_q;
+
+// Every work item should have a context struct, where k_work work is a member.
+// this allows for other data to be passed to the work handler. Use macros below
+// to avoid referencing the k_work item maually.
+/*
+static struct somework_context {
+        struct k_work work;
+        ...
+} work1;
+
+static void somework_task(struct k_work *work)
+{
+    struct somework_context *context = CONTAINER_OF(work, struct somework_context, work);
+    LOG_DBG("somework_task");
+    ...
+
+}
+    work_init(somework, womework_task);
+
+    work_submit(somework);
+
+*/
+
+#define work_submit(context) \
+    k_work_submit_to_queue(&work_q, &context.work)
+#define work_init(context, fn) \
+    k_work_init(&context.work, fn)
+
+static struct work1_context {
+        struct k_work work;
+} work1;
+
+static void work1_task(struct k_work *work)
+{
+    struct work1_context __unused *context = CONTAINER_OF(work, struct work1_context, work);
+    LOG_DBG("work1_task");
+}
+
+static void module_initialize(void)
+{
+    LOG_DBG("initialize");
+    k_work_queue_start(&work_q, stack_area, K_THREAD_STACK_SIZEOF(stack_area),
+                    CONFIG_SYSTEM_WORKQUEUE_PRIORITY, NULL); // TODO: Assign proper priority, and what is NULL?
+    work_init(work1, work1_task);
+    module_set_state(MODULE_STATE_READY);
+    LOG_DBG("initialized");
+}
+
 #if CONFIG_CAF_BUTTON
 static bool handle_button_event(const struct button_event *evt)
 {
@@ -39,8 +90,8 @@ static bool handle_module_state_event(const struct module_state_event *evt)
 {
     // Wait for dependent modules to initialize
 	if (check_state(evt, MODULE_ID(main), MODULE_STATE_READY)) {
-        // Initialize
-        module_set_state(MODULE_STATE_READY);
+        // Initialize system parts
+        module_initialize();
     }
 	return false;
 }
@@ -73,8 +124,7 @@ APP_EVENT_SUBSCRIBE(MODULE, button_event);
 #if CONFIG_SHELL
 #include <zephyr/shell/shell.h>
 
-static int test_1_echo(const struct shell *shell, size_t argc,
-		char **argv)
+static int sh_echo(const struct shell *shell, size_t argc, char **argv)
 {
     for (int i = 0; i < argc; i++) {
 	    shell_fprintf(shell, SHELL_NORMAL, "echo '%s'\n", argv[i]);    
@@ -82,11 +132,18 @@ static int test_1_echo(const struct shell *shell, size_t argc,
     return 0;
 }
 
-SHELL_STATIC_SUBCMD_SET_CREATE(sub_test_1,
-	SHELL_CMD_ARG(echo, NULL, "Echo", test_1_echo, 0, 0),
+static int sh_work(const struct shell *shell, size_t argc, char **argv)
+{
+    work_submit(work1);
+    return 0;
+}
+
+SHELL_STATIC_SUBCMD_SET_CREATE(module_shell,
+	SHELL_CMD_ARG(echo, NULL, "Echo", sh_echo, 0, 0),
+	SHELL_CMD_ARG(work, NULL, "work", sh_work, 0, 0),
 	SHELL_SUBCMD_SET_END
 );
 
-SHELL_CMD_REGISTER(MODULE, &sub_test_1, "Test_1 commands", NULL);
+SHELL_CMD_REGISTER(MODULE, &module_shell, "Test 1 commands", NULL);
 
 #endif
